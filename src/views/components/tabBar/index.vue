ba64d4d9b68e04f86bb28f5b3739a871f368f3fd
wqb2017 <542672187@qq.com>
2018-08-10 15:23:02 +0800

    新增：个人中心mime；首页home

--- src/views/components/tabBar/index.vue
+++ src/views/components/tabBar/index.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="tabnav">
     <van-tabbar v-model="active">
-      <van-tabbar-item icon="wap-home">首页</van-tabbar-item>
-      <van-tabbar-item icon="contact">个人中心</van-tabbar-item>
+      <van-tabbar-item icon="wap-home" to="/home">首页</van-tabbar-item>
+      <van-tabbar-item icon="contact" to="/mime">个人中心</van-tabbar-item>
     </van-tabbar>
   </div>
 </template>
