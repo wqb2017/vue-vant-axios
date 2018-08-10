5edcefd8908f4caee4bedbb2e935804bd4fd8ab8
wqb2017 <542672187@qq.com>
2018-08-10 15:37:33 +0800

    px转rem

--- .postcssrc.js
+++ .postcssrc.js
@@ -5,6 +5,12 @@ module.exports = {
     "postcss-import": {},
     "postcss-url": {},
     // to edit target browsers: use "browserslist" field in package.json
-    "autoprefixer": {}
+    'autoprefixer': {
+      browsers: ['Android >= 4.0', 'iOS >= 7']
+    },
+    'postcss-pxtorem': {
+      rootValue: 100,
+      propList: ['*']
+    }
   }
 }
